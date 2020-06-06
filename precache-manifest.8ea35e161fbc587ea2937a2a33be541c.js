1367c6c40b8c2a463bb89437c22a10a66d752f73
Nikhil Chakravartula <nikhilchakravartula@gmail.com>
2020-06-06 19:11:08 +0530

    Updates

---
+++ precache-manifest.8ea35e161fbc587ea2937a2a33be541c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b71a48aa0132df466a053099f2edd03",
+    "url": "/PortFolio/index.html"
+  },
+  {
+    "revision": "c8aa39688a34d7e1284d",
+    "url": "/PortFolio/static/css/main.18f3022c.chunk.css"
+  },
+  {
+    "revision": "51d4bf7c95abe462fca4",
+    "url": "/PortFolio/static/js/2.d16d183f.chunk.js"
+  },
+  {
+    "revision": "71772b085033e92f89cdfb3071333458",
+    "url": "/PortFolio/static/js/2.d16d183f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8aa39688a34d7e1284d",
+    "url": "/PortFolio/static/js/main.04ab946f.chunk.js"
+  },
+  {
+    "revision": "26f746a90f32bff32364",
+    "url": "/PortFolio/static/js/runtime-main.8dc53c33.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/PortFolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "c32099636d6e08e8e678d89c082facef",
+    "url": "/PortFolio/static/media/BeingZeroLogo.c3209963.JPG"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/PortFolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/PortFolio/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/PortFolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/PortFolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/PortFolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/PortFolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "de3da8064f2e9ffab945772c2c67e46f",
+    "url": "/PortFolio/static/media/talksCardBack.de3da806.svg"
+  },
+  {
+    "revision": "e7d73c8e3af1d7422819914feb314a57",
+    "url": "/PortFolio/static/media/teradata_logo.e7d73c8e.JPG"
+  }
+]);
